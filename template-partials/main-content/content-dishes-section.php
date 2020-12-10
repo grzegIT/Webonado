ad61e5c1539dd88136beab498fb205e604db5828
Grzegorz Golonka <grzeg.it@gmail.com>
2020-12-10 07:49:18 +0100

    expanded a structure of the dishes section to project requirements

--- template-partials/main-content/content-dishes-section.php
+++ template-partials/main-content/content-dishes-section.php
@@ -24,52 +24,77 @@
                 ?>  
             </div>
             
-            <div class="product-list__items" style="display: flex; flex-direction: row; flex-wrap: wrap;">
+            <div class="product-list__items">
                 
                 <?php
                     query_posts('cat=1');
                     while (have_posts()) : the_post(); 
                 ?>
-                    <div class='item'>
-                        
-                        <a href="<?php the_field('item-name');?>?>">
-                            <?php the_post_thumbnail(); ?>
-                        </a>
+                    <div class='list__item'>
 
                         <!-- HIT -->
-                        <?php if(the_field('hit')) : ?>
-                                <span class="item--hit"><?php the_field('hit');?></span>
-                        <?php endif; ?>
+                        
+                        <?php 
+                        $hit = get_field('hit');
+                        if($hit): ?>
+                                <p class="item__hit"><?php echo the_field('hit');?></p>
+                        <?php endif;?></p>
+                        
+                        
+                        <a href="<?php the_permalink();?>">
+                           <img class="item__img" src="<?php the_field('item-image'); ?>" alt="<?php the_field('item-image')['alt']; ?>">
+                        </a>
 
-                        <div class="item_description">
-                            <a href="<?php esc_url(the_field('item-name'));?>">
-                                <h4 class='item__title'>
+                        
+                        <div class="item__description">
+                            
+                            <h4 class='item__title'>
+                                <a href="<?php the_permalink();?>">
                                     <?php the_field('item-name'); ?>
-                                </h4>
-                            </a>
-                            <p>
+                                </a>
+                            </h4>
+                            
+                            <h5>
                                 <?php the_field('item-size');?>
-                            </p>
+                            </h5>
+                            
                         </div>
-                        <div class="item_ingredients">
+                        <div class="item__ingredients">
                             <?php the_field('ingredients');?>
                         </div>
-                        <h3>
-                            <?php the_field('price');?> USD
-                        </h3>
+                        
+                        <h3><?php the_field('price');?> USD</h3>
+                        
+                        <a href="<?php the_permalink(); ?>">
+                            <div class="item__buy">
+                                <img src="<?php echo bloginfo('template_url').'/assets/icons/utility-icons/buy-icon--active.svg'?>">
+                            </div>
+                        </a>
                         
                     </div>
                 <?php
                     endwhile;
-                   ?>
+                ?>
                 
 
             </div>
         </div>   
 
         <a href="<?php echo esc_url( home_url( '/more_products' ) );?>">
-            <img src="<?php echo bloginfo('template_url') . './assets/icons/utility-icons/more-icon.svg' ?>" alt="<?php echo the_title();?>">
+            <img class="item-more" src="<?php echo bloginfo('template_url') . './assets/icons/utility-icons/more-icon.svg' ?>" alt="<?php echo the_title();?>">
         </a>
+
+        <img src="<?php echo bloginfo('template_url') . './assets/icons/food-icons/meat-icon.svg'?>" alt="" class="absolute-class meat_icon">
+    
+        <img src="<?php echo bloginfo('template_url') . './assets/icons/food-icons/tongs-icon.svg'?>" alt="" class="absolute-class tongs_icon">
+    
+        <img src="<?php echo bloginfo('template_url') . './assets/icons/food-icons/broccoli-icon.svg'?>" alt="" class="absolute-class broccoli_icon">
     </div>
+    
+    
+    
+    
+        
+    
 </section>
 
